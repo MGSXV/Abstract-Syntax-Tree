87932e4433bcaec2cd2cff03dff60e1330bbbc29
Soufiane Elkahmlichi <05soufiane@gmail.com>
2022-07-01 01:07:47 +0100

    Declare functions in header file

--- includes/headers/interpreter.h
+++ includes/headers/interpreter.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 17:26:43 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/29 00:59:17 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/06/30 23:15:55 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,10 @@ typedef struct s_interpreter
 	int		index;
 	t_token	*current_token;
 }	t_interpreter;
+
+t_interpreter	*init_interpreter(char *eq);
+t_token			*get_next_token(t_interpreter *intrp);
+bool			check_token(int token_code, t_token *token);
+int				calculate(char *problem);
 
 #endif
